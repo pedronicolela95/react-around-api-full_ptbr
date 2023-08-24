88eee69e84aa216a3c325b186d594b3a22aa326a
Pedro Nicolela <nicolela.pedro@gmail.com>
2023-08-23 22:14:52 -0300

    test

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-var cors = require("cors");
+const cors = require("cors");
 
 const { PORT = 3000 } = process.env;
 
