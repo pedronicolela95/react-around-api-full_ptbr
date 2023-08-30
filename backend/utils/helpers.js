c6ff7ed66ae5955106fe390f7d9a3940f9121289
Pedro Nicolela <nicolela.pedro@gmail.com>
2023-08-30 09:29:11 -0300

    fix

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,18 +1,25 @@
 const { NODE_ENV, JWT_SECRET } = process.env;
 const validator = require("validator");
 
-export const secret = NODE_ENV === "production" ? JWT_SECRET : "dev-secret";
+const secret = NODE_ENV === "production" ? JWT_SECRET : "dev-secret";
 
-export function isValidAvatarURL(v) {
-  const regex = /^https?:\/\/(www\.)?[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}/;
+function isValidAvatarURL(v) {
+  const regex =
+    /^https?:\/\/(www\.)?[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}/;
   if (!regex.test(v)) {
     throw new Error(`${v} não é uma URL válida`);
   }
 }
 
-export function validateURL(value, helpers) {
+function validateURL(value, helpers) {
   if (validator.isURL(value)) {
     return value;
   }
   return helpers.error("string.uri");
 }
+
+module.exports = {
+  secret,
+  isValidAvatarURL,
+  validateURL,
+};
